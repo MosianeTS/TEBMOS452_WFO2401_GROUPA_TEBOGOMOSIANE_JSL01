4d21cb8ca9fc2cfe39bb840b9576471f972fa522
Tebogo Mosiane <mosianets@gmail.com>
2024-03-13 16:13:56 +0200

    Updated validation logic

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
+
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    let result1 = ''; 
+    let result2 = '';
+    let result3 = '';
+    let result = '';
 
-            document.getElementById('result').innerText = result;
+    if (input[0] == "p" && input[1] == "e" && input[2] == "t" && input[3] == "_") {
+        result1 = "part 1 good";
+    }
+    else {
+        result1 = "part 1 not good";        
+    }  
+
+    if (!isNaN(input[4]) && !isNaN(input[5]) && !isNaN(input[6]) && !isNaN(input[7]) ) {
+        result2 = 'part 2 good';
+    } else {
+        result2 = 'part 2 NOT good';
+    }
+    
+    if (/[a-zA-Z]/.test(input[8]) == true && /[a-zA-Z]/.test(input[9]) == true && /[a-zA-Z]/.test(input[10]) == true && /[a-zA-Z]/.test(input[11]) == true) {
+        result3 = 'part 3 good';
+    }
+    else {
+        result3 = 'part 3 not good';
+    }
+    
+    
+    if (result1 == "part 1 good" && result2 == 'part 2 good' && result3 == 'part 3 good') {
+        result = 'Valid syntax'
+    }
+    else {
+        result = "Invalid syntax"
+    }
+
+    document.getElementById('result').innerText = result;
 }
 
 
